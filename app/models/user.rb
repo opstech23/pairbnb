f1401012de1f74654d6614c50dd082f27dd5b9e3
opstech23 <bbody8215@gmail.com>
2017-10-25 20:35:51 +0800

    added oauth, key auth and rack middleware

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ApplicationRecord
   include Clearance::User
+  has_many :authentications, dependent: :destroy
 
+  def self.create_aith_auth_and_hash(authentication, auth_hash)
+    user = self.create!(
+      name: auth_hash["name"],
+      email: auth_hash["extra"]["raw_info"]["email"]
+    )
+
+    user.authentications << authentication
+    return user
+  end
+
+  def fb_token
+    x = self.authentications.find_by(provider: 'facebook')
+    return x.token unless x.nil?
 end
