d6d13fee1ed3698cf5bd25fffdedf281d792ec20
opstech23 <bbody8215@gmail.com>
2017-10-26 11:13:23 +0800

    adding listing capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   include Clearance::User
   has_many :authentications, dependent: :destroy
 
-  def self.create_aith_auth_and_hash(authentication, auth_hash)
+  def self.create_with_auth_and_hash(authentication, auth_hash)
     user = self.create!(
       name: auth_hash["name"],
       email: auth_hash["extra"]["raw_info"]["email"]
@@ -15,4 +15,5 @@ class User < ApplicationRecord
   def fb_token
     x = self.authentications.find_by(provider: 'facebook')
     return x.token unless x.nil?
+  end
 end
