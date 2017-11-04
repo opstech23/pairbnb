bbb98119e8d0a419d5af5fcdee193759bcee3126
opstech23 <bbody8215@gmail.com>
2017-11-04 14:18:04 +0800

    edit listing feature can now populate the form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,10 +2,28 @@ class ListingsController < ApplicationController
   before_action :require_login
 
   def index
-    current_user.listings
+    # current_user.listings
+    @listings = Listing.all
   end
 
   def new
-  	@listings = Listing.new
+  	@listing = Listing.new
   end
+
+  def create
+  	@listing = Listing.new(listing_params)
+  	if @listing.save
+  		redirect_to listings_path
+  	end
+  end
+
+  def edit
+  	@listing = Listing.find(params[:id])
+  end
+
+  private
+  	def listing_params
+  		params.require(:listing).permit(:title, :description, :country,
+  			:state_or_province, :max_guests, :price)
+  	end
 end
